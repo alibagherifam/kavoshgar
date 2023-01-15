02d60e39652cf2a8a05f2416079a9685a7d7410e
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-01-16 03:02:02 +0330

    Add documentation to ServerInformation

--- lib/src/main/kotlin/com/alibagherifam/kavoshgar/lobby/ServerInformation.kt
+++ lib/src/main/kotlin/com/alibagherifam/kavoshgar/lobby/ServerInformation.kt
@@ -2,10 +2,20 @@ package com.alibagherifam.kavoshgar.lobby
 
 import java.net.InetAddress
 
+/**
+ * Data model which contains information that will be shown to clients.
+ *
+ * @param[name] an arbitrary name.
+ * @param[address] internet Protocol address of server.
+ * @param[latency] round-trip time for messages sent to server.
+ */
 data class ServerInformation(
     val name: String,
     val address: InetAddress,
     val latency: Long
 ) {
+    /**
+     * @return server address in dot-decimal notation (e.g., 192.168.10.64).
+     */
     val addressName: String get() = address.toString().drop(1)
 }
