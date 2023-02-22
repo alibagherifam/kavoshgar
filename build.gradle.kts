70fa76ce2d7e67f90ddb76690e6131969d6e858e
Ali Bagherifam <ali.bagherifam@gmail.com>
2023-02-22 16:23:41 +0330

    Add dependencyVersion plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.dependencyVersions)
 }
 
 tasks.register<Delete>("clean") {
