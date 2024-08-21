83e12a088bd288a1bb728aeea27d9910ac6a0fdf
Ali Bagherifam <ali.bagherifam@snapp.cab>
2024-08-22 00:28:31 +0330

    Bump compose multiplatform

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.kotlin.multiplatform)
 }
 
 kotlin {
