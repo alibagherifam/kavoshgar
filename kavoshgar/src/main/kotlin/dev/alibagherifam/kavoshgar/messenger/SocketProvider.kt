7b4d5793e60f8b16d316d515e02ab57f424dd250
Ali Bagherifam <ali.bagherifam@gmail.com>
2025-02-20 23:39:32 +0330

    Format code

--- kavoshgar/src/main/kotlin/dev/alibagherifam/kavoshgar/messenger/SocketProvider.kt
+++ kavoshgar/src/main/kotlin/dev/alibagherifam/kavoshgar/messenger/SocketProvider.kt
@@ -1,7 +1,26 @@
 package dev.alibagherifam.kavoshgar.messenger
 
+import dev.alibagherifam.kavoshgar.Constants
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.ServerSocket
 import java.net.Socket
 
 interface SocketProvider {
     fun openSocket(): Socket
+}
+
+class ServerSocketProvider : SocketProvider {
+    override fun openSocket(): Socket {
+        val serverSocket = ServerSocket(Constants.MESSAGING_PORT)
+        return serverSocket.accept()
+    }
+}
+
+class ClientSocketProvider(
+    private val serverAddress: InetAddress
+) : SocketProvider {
+    override fun openSocket(): Socket = Socket().apply {
+        connect(InetSocketAddress(serverAddress, Constants.MESSAGING_PORT))
+    }
 }
