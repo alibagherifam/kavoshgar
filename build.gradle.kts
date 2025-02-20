aab6177be669905496db2f6dba4864cab962d4f2
Ali Bagherifam <ali.bagherifam@gmail.com>
2025-02-21 01:33:52 +0330

    Format code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.versions)
 }
 
 tasks.register<Delete>("clean") {
